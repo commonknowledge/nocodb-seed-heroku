82f64d00c67ed8cfa9fa2a09688fbcc81b7b7713
Alex Worrad-Andrews <alex@alexandrews.info>
2025-06-02 14:06:03 +0100

    Write GitHub deploy key before use

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 set -e
 
-# Optional: set up Git SSH
-if [ -n "$DEPLOY_KEY" ]; then
+# Set up Git SSH
+if [ -n "$GITHUB_DEPLOY_KEY_PRIVATE" ]; then
   mkdir -p ~/.ssh
-  echo "$DEPLOY_KEY" > ~/.ssh/id_rsa
+  echo "$GITHUB_DEPLOY_KEY_PRIVATE" > ~/.ssh/id_rsa
   chmod 600 ~/.ssh/id_rsa
   ssh-keyscan github.com >> ~/.ssh/known_hosts
 fi
 
-# Clone the repo if it doesn’t exist
+# Clone the repo if it doesn't exist
 if [ ! -d "/usr/src/appEntry/repo/.git" ]; then
   echo "[entrypoint] Cloning Git repo..."
   git clone git@github.com:commonknowledge/community-climate-justice-archive.git /usr/src/appEntry/repo
