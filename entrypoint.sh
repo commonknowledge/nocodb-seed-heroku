444565ff7ff958cb69b841725ebfb96d82cacb9c
Alex Worrad-Andrews <alex@commonknowledge.coop>
2025-09-22 10:49:29 +0100

    Disable Dudley version (for the moment)

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,21 +2,21 @@
 set -e
 
 # Set up Git SSH
-if [ -n "$GITHUB_DEPLOY_KEY_PRIVATE" ]; then
-  mkdir -p ~/.ssh
-  echo "$GITHUB_DEPLOY_KEY_PRIVATE" > ~/.ssh/id_rsa
-  chmod 600 ~/.ssh/id_rsa
-  ssh-keyscan github.com >> ~/.ssh/known_hosts
-fi
+# if [ -n "$GITHUB_DEPLOY_KEY_PRIVATE" ]; then
+#   mkdir -p ~/.ssh
+#   echo "$GITHUB_DEPLOY_KEY_PRIVATE" > ~/.ssh/id_rsa
+#   chmod 600 ~/.ssh/id_rsa
+#   ssh-keyscan github.com >> ~/.ssh/known_hosts
+# fi
 
-# Clone the repo if it doesn't exist
-if [ ! -d "/usr/app/data/repo/.git" ]; then
-  echo "[entrypoint] Cloning Git repo..."
-  git clone git@github.com:commonknowledge/community-climate-justice-archive.git /usr/app/data/repo
-fi
+# # Clone the repo if it doesn't exist
+# if [ ! -d "/usr/app/data/repo/.git" ]; then
+#   echo "[entrypoint] Cloning Git repo..."
+#   git clone git@github.com:commonknowledge/community-climate-justice-archive.git /usr/app/data/repo
+# fi
 
-# Start sync in background
-/usr/src/appEntry/sync-sqlite-to-git.sh &
+# # Start sync in background
+# /usr/src/appEntry/sync-sqlite-to-git.sh &
 
 # Start NocoDB
 exec sh /usr/src/appEntry/start.sh
